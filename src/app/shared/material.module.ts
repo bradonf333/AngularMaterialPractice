9cd54286066c103264f0a22c95540ea365c780ea
bradonf333 <bradonf123@gmail.com>
2018-02-27 21:22:01 -0700

    Import 2 material modules

--- src/app/shared/material.module.ts
+++ src/app/shared/material.module.ts
@@ -1,5 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {
+  MatButtonModule,
+  MatCheckboxModule
+} from '@angular/material';
 
 @NgModule({
   imports: [
