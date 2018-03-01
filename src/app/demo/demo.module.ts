813c3d2b33fb43dc9c79a1a567d4828c92d24cbd
bradonf333 <bradonf123@gmail.com>
2018-02-28 18:30:45 -0700

    Separate buttons to own component, routing $ blank flexbox comp

---
+++ src/app/demo/demo.module.ts
@@ -0,0 +1,19 @@
+import { FormsModule } from '@angular/forms';
+import { MaterialModule } from './../shared/material.module';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { DemoRoutingModule } from './demo-routing.module';
+import { ButtonsComponent } from './buttons/buttons.component';
+import { FlexboxComponent } from './flexbox/flexbox.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    DemoRoutingModule,
+    MaterialModule,
+    FormsModule
+  ],
+  declarations: [ButtonsComponent, FlexboxComponent]
+})
+export class DemoModule { }
